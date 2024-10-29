f7db1d88ce4d329077b92415ddb983a524449636
yuna-18 <huraigon10@gmail.com>
2024-10-30 00:53:39 +0900

    戦闘画面突入までざっくり

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(function () {
+  $(".pay-btn").on("click", function () {
+    $(".dialog__container").addClass("is-active");
+  });
+  $(".dialog-btn.close-btn, .dialog-btn.no").on("click", function () {
+    $(".dialog__container").removeClass("is-active");
+  });
+  
+  $(".dialog-btn.yes").on("click", function () {
+    // モード選択非表示・ダイアログ非表示
+    $(".pay__container").removeClass("is-active");
+    $(".dialog__container").removeClass("is-active");
+    
+  });
+})
